42bc4845db7f90863e6d1f2d296a7db99a826195
Dima Koskin <dmkskn@dmkskn.com>
2022-11-13 18:48:33 +0300

    Remove fatalError check

--- Tests/PlatformSpecificValueTests/PlatformSpecificValueTests.swift
+++ Tests/PlatformSpecificValueTests/PlatformSpecificValueTests.swift
@@ -2,7 +2,6 @@ import XCTest
 @testable import PlatformSpecificValue
 
 
-
 final class PlatformSpecificValueTests: XCTestCase {
     func testThatPlatformSpecificFunctionReturnsTheRightValue() throws {
         let value: String = platformSpecific([.macOS("macOS"), .iOS("iOS"), .watchOS("watchOS"), .tvOS("tvOS")])
@@ -19,24 +18,4 @@ final class PlatformSpecificValueTests: XCTestCase {
         #error("Testing on an unsupported platform")
         #endif
     }
-    
-    func testThatPlatformSpecificFunctionThrowsFatalErrorIfThereIsNoAppropriateValue() throws {
-        let values: Set<PlatformSpecificValue<String>> = [.macOS("macOS"), .iOS("iOS"), .watchOS("watchOS"), .tvOS("tvOS")]
-        
-        XCTestFatalError("No value was provided.") {
-            #if os(macOS)
-            let filteredValues = values.filter { $0 != .macOS("macOS") }
-            #elseif os(iOS)
-            let filteredValues = values.filter { $0 != .iOS("iOS") }
-            #elseif os(watchOS)
-            let filteredValues = values.filter { $0 != .watchOS("watchOS") }
-            #elseif os(tvOS)
-            let filteredValues = values.filter { $0 != .tvOS("tvOS") }
-            #else
-            #error("Testing on an unsupported platform")
-            #endif
-            _ = platformSpecific(filteredValues)
-        }
-    }
 }
-
